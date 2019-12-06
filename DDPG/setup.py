ffea4f54dc8c2f9b53a21c61854298e13b6a3ebe
Dekki <47814095+Dekki-Aero@users.noreply.github.com>
2019-12-06 17:12:57 +0530

    Delete setup.py

--- DDPG/setup.py
+++
@@ -1,7 +0,0 @@
-from setuptools import setup
-
-setup(name='DDPG',
-      version = '0.1',
-      author='Dekki',
-      author_email = 'akhil4dp@gmail.com',
-      install_requires=['tensorflow==2.0','gym','numpy','matplotlib'])
